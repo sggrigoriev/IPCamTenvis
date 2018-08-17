49bc3b07d72bf29935f684162aa727ce71f6ac44
sggrigoriev <sggrigoriev@gmail.com>
2018-08-17 21:41:06 +0300

    #include <unistd.h> changed to #include <getopt.h>

--- main.c
+++ main.c
@@ -23,7 +23,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
-#include <unistd.h>
+#include <getopt.h>
 
 #include <curl/curl.h>  /* for global init/deinit */
 #include <gst/gst.h>    /* for gloal init/deinit */
