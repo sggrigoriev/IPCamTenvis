9a9a55d750def6b8574714af51151cccce765b58
sggrigoriev <sggrigoriev@gmail.com>
2018-08-17 21:27:14 +0300

    #include <unistd.h> added

--- main.c
+++ main.c
@@ -23,6 +23,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <unistd.h>
 
 #include <curl/curl.h>  /* for global init/deinit */
 #include <gst/gst.h>    /* for gloal init/deinit */
